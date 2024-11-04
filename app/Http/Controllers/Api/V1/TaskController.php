3609225b4ae728ca4d4a197970abaf124a8c8abb
Aurnab <zubayeraurnab@gmail.com>
2024-11-04 21:38:14 +0600

    post api created

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Resources\TaskResource;
 use App\Models\Task as ModelsTask;
 use Illuminate\Http\Request;
 
@@ -13,31 +15,35 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return ModelsTask::all();
+        return TaskResource::collection(ModelsTask::all());
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
+    // public function create()
+    // {
+
+    // }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreTaskRequest $request)
     {
-        //
+        // Create the task using validated request data
+        $task = ModelsTask::create($request->validated());
+
+        // Return the created task as a resource
+        return TaskResource::make($task);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(ModelsTask $task)
     {
-        //
+        return TaskResource::make($task);
     }
 
     /**
